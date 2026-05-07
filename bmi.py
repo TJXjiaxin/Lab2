ea2f1fc344e382f8c264759360165780a4822136
Jia Xin <tjxjia.25@ichat.sp.edu.sg>
2026-05-07 13:55:03 +0800

    update

--- bmi.py
+++ bmi.py
@@ -1,47 +1,18 @@
 
-def main():
-    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-    display_main_menu()
-    num_list = get_user_input()
-    calc_average_temperature(num_list)
-    calc_min_max_temperature(num_list)
-    calc_median_temperature(num_list)
-    
-    
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
 
-def display_main_menu():
-    print("Enter temperatures")
+    bmi = weight / height ** 2
+    print("BMI = " + str(round(bmi, 2)))
 
-
-def get_user_input():
-    user_input = input("Enter numbers: ")
-    number = user_input.split(",")
-    num =[]
-    for i in number:
-        num.append(float(i))
-    return num
-
-def calc_average_temperature(num_list) :
-    a = sum(num_list) / len(num_list)
-    print(a)
-
-def calc_min_max_temperature(num_list):
-   mini=min(num_list)
-   maxi=max(num_list)
-   print ("min ="+str(mini))
-   print ("max ="+str(maxi))
-
-
-def calc_median_temperature (num_list):
-    orderlist=sorted(num_list)
-    i=len(orderlist)
-    if i%2 ==0:
-        median=(orderlist[i//2]+ orderlist[i//2-1])/2
+    if bmi < 18.5:
+        return -1
+    elif bmi < 24.9:
+        return 0
     else:
-        median=orderlist[i//2]
-    print("median =" + str(median))
-   
-if __name__ == "__main__":
- main()
+        return 1
 
 
+result = calculate_bmi(weight=57, height=1.73)
+print(result)
